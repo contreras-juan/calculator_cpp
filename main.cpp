56c0ed6cfcab9328f2db5e687bf39e197f79b596
Juan Felipe Contreras <j.contrerasa94@gmail.com>
2025-07-19 15:04:02 -0500

    FEATURE: Agregar uso básico de calculadora en main.cpp

--- main.cpp
+++ main.cpp
@@ -6,10 +6,30 @@
 
 int main()
 {
-    std::cout << "Summation: " << add(2,2) << '\n';
-    std::cout << "Substraction: " << substract(2,2) << '\n';
-    std::cout << "Multiplication: " << multiply(2,2) << '\n';
-    std::cout << "Division: " << divide(2,2) << '\n';
+    float a {};
+    float b {};
+    char operation {};
+
+    std::cout << "Enter first number: ";
+    std::cin >> a;
+
+    std::cout << "Enter second number: ";
+    std::cin >> b;
+
+    std::cout << "Enter operation (+, -, *, /): ";
+    std::cin >> operation;
+
+    if (operation == '+') {
+        std::cout << add(a, b) << '\n';
+    } else if (operation == '-') {
+        std::cout << substract(a, b) << '\n';
+    } else if (operation == '*') {
+        std::cout << multiply(a, b) << '\n';
+    } else if (operation == '/') {
+        std::cout << divide(a, b) << '\n';
+    } else {
+        std::cout << "Operation not valid. Choose from +, -, *, /" << '\n';
+    }
 
     return 0;
 }
